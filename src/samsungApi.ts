5da109cd8b7a1a017208a88897f89cdffbf7e086
Irinel Nistor <irinel.nistor@consultant.lego.com>
2021-07-06 15:54:03 +0300

    Fix host string

--- src/samsungApi.ts
+++ src/samsungApi.ts
@@ -9,7 +9,7 @@ export class SamsungAPI {
   }
 
   static async getDevices(token) {
-    const { data: { items = [] } = {} } = await Axios.get('${HOST}', this.setToken(token));
+    const { data: { items = [] } = {} } = await Axios.get(`${HOST}`, this.setToken(token));
 
     return items.filter(item => item.name === AC_DEVICE_NAME);
   }
